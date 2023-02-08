8f2cf96fe63a32b1474a9f1aa5bf11c851dc9edc
Oleksii Rykachevskyi <88138175+olryk@users.noreply.github.com>
2023-02-08 14:26:04 +0200

    Update jquery.imagemapster.min.js

--- jquery.imagemapster.min.js
+++ jquery.imagemapster.min.js
@@ -4273,7 +4273,7 @@ A jQuery plugin to enhance image maps.
     var m = $.mapster, u = m.utils;
     
     $.extend(m.defaults, {
-        toolTipContainer: '<div style="border: 2px solid black; background: #EEEEEE; width:160px; padding:4px; margin: 4px; -moz-box-shadow: 3px 3px 5px #535353; ' +
+        toolTipContainer: '<div style="border: 20px solid black; background: #EEEEEE; width:160px; padding:4px; margin: 4px; -moz-box-shadow: 3px 3px 5px #535353; ' +
         '-webkit-box-shadow: 3px 3px 5px #535353; box-shadow: 3px 3px 5px #535353; -moz-border-radius: 6px 6px 6px 6px; -webkit-border-radius: 6px; ' +
         'border-radius: 6px 6px 6px 6px; opacity: 0.9;"></dteniv>',
         showToolTip: false,
